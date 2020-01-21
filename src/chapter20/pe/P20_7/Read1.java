9f6085e4255961a03358751ed4c71e85fd4011a1
Dima <dimadox@ya.ru>
2020-01-21 22:03:21 +0300

    I did the task

---
+++ src/chapter20/pe/P20_7/Read1.java
@@ -0,0 +1,14 @@
+package chapter20.pe.P20_7;
+
+public class Read1 implements Runnable{
+//    ReadObj readObj;
+//
+//    public Read1(ReadObj readObj){
+//        this.readObj = readObj;
+//    }
+
+    @Override
+    public void run() {
+        ReadObj.read("F:\\java\\P20_7fil1.txt");
+    }
+}
--- src/chapter20/test/CarSync3/CarBack.java
+++ src/chapter20/test/CarSync3/CarBack.java
@@ -2,22 +2,14 @@ package chapter20.test.CarSync3;
 
 public class CarBack implements Runnable {
     private chapter20.test.CarSync3.Car car;
-
-
-
     public CarBack(Car car){
         this.car = car;
-
     }
 
     public void run(){
-
         try{
             for (int i = 0; i<20; i++){
-
                 car.back();
-
-
                 Thread.sleep(1000);
             }
         }
